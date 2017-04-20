33330af3c6618941dc26430e503d407100e8edf0
L. Lam <leslielam@ucla.edu>
2017-04-20 15:30:58 -0700

    Basic Android Studio commit

---
+++ Assassin/app/src/main/java/com/example/aria/assassin/Launch.java
@@ -0,0 +1,19 @@
+package com.example.aria.assassin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Launch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+
+    /** Called when the user taps the Enter button for Username*/
+    public void setUsername(View view) {
+        // Do something in response to button
+    }
+}
