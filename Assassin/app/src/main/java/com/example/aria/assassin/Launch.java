60c592ec85c19ad87f6778a1b28a30a2c141e15d
L. Lam <leslielam@ucla.edu>
2017-04-25 16:48:03 -0700

    Add username validation Add basic player list and Exit Lobby / Start Game buttons

--- Assassin/app/src/main/java/com/example/aria/assassin/Launch.java
+++ Assassin/app/src/main/java/com/example/aria/assassin/Launch.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Launch extends AppCompatActivity {
     public static final String EXTRA_USERNAME = "com.example.aria.assassin";
@@ -17,10 +21,37 @@ public class Launch extends AppCompatActivity {
 
     /** Called when the user taps the Enter button for Username*/
     public void setUsername(View view) {
-        Intent intent = new Intent(this, LobbyGM.class);
+        // Get the username entered
         EditText usernameField = (EditText) findViewById(R.id.usernameField);
         String username = usernameField.getText().toString();
-        intent.putExtra(EXTRA_USERNAME, username);
-        startActivity(intent);
+
+        if (!username.isEmpty() && isValidUsername(username)) {
+            Intent intent;
+            // Check server if a lobby already exists
+            boolean isGM = true;
+
+            if (isGM) {
+                // If no lobby, then enter as GM
+                intent = new Intent(this, LobbyGM.class);
+            }
+            else {
+                // If lobby exists, then enter as reular
+                intent = new Intent(this, LobbyRegular.class);
+            }
+
+            // Enter the lobby
+            intent.putExtra(EXTRA_USERNAME, username);
+            startActivity(intent);
+        }
+        else {
+            usernameField.setError("Usernames can only contain the following characters: alphanumeric, ., -, _, ~");
+        }
+    }
+
+    // Usernames can only contain alphanumeric, ".", "-", "_", "~"
+    private boolean isValidUsername(String username) {
+        Pattern pattern = Pattern.compile("[^a-zA-Z0-9\\.\\-\\_~]");
+        Matcher matcher = pattern.matcher(username);
+        return !matcher.find();
     }
 }
