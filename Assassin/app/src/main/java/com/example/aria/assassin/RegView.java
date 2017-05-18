720d845f99cdd6d292b58134ffd9080f2a162a68
L. Lam <leslielam@ucla.edu>
2017-05-18 15:41:43 -0700

    Refactor code to make it cleaner

--- Assassin/app/src/main/java/com/example/aria/assassin/RegView.java
+++ Assassin/app/src/main/java/com/example/aria/assassin/RegView.java
@@ -1,5 +1,7 @@
 package com.example.aria.assassin;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,29 +10,74 @@ import android.view.View;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResult;
-
-/**
- * Created by Lauren on 4/25/2017.
- */
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 public class RegView extends AppCompatActivity implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
-        com.google.android.gms.location.LocationListener {
-    private final int LOCATION_REQUEST_INTERVAL = 5000; // Query every 5 seconds
-    private final int LOCATION_REQUEST_INTERVAL_MAX = 3000; // Maximum rate of every 3 seconds
+        ConnectionCallbacks,
+        OnConnectionFailedListener,
+        LocationListener,
+        ResultCallback<LocationSettingsResult> {
 
-    private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
+    protected static final String LOCATION_TAG = "RegView.LOCATION";
+
+    /**
+     * Constant used in the location settings dialog.
+     */
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+
+    /**
+     * Interval for location updates in milliseconds
+     */
+    public final int LOCATION_REQUEST_INTERVAL = 5000; // Query every 5 seconds
+
+    /**
+     * The fastest rate for active location updates. Exact. Updates will never be more frequent
+     * than this value.
+     */
+    public final long LOCATION_REQUEST_INTERVAL_MAX = LOCATION_REQUEST_INTERVAL/2;
+
+    /**
+     * Provides the entry point to Google Play services.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+
+    /**
+     * Stores parameters for requests to the FusedLocationProviderApi.
+     */
+    protected LocationRequest mLocationRequest;
+
+    /**
+     * Stores the types of location services the client is interested in using. Used for checking
+     * settings to determine if the device has optimal location settings.
+     */
+    protected LocationSettingsRequest mLocationSettingsRequest;
+
+    /**
+     * Represents a geographical location.
+     */
+    protected Location mCurrentLocation;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regview);
 
+        buildGoogleApiClient();
+        createLocationRequest();
+        buildLocationSettingsRequest();
+        checkLocationSettings();
+    }
+
+    protected synchronized void buildGoogleApiClient() {
         // Create an instance of GoogleAPIClient.
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -38,27 +85,89 @@ public class RegView extends AppCompatActivity implements
                     .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
+        }
+    }
 
-            // Set location request parameters
-            mLocationRequest = new LocationRequest();
-            mLocationRequest.setInterval(LOCATION_REQUEST_INTERVAL);
-            mLocationRequest.setFastestInterval(LOCATION_REQUEST_INTERVAL_MAX);
-            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-            LocationSettingsRequest.Builder locationBuilder = new LocationSettingsRequest.Builder()
-                    .addLocationRequest(mLocationRequest);
+    protected void createLocationRequest() {
+        // Set location request parameters
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(LOCATION_REQUEST_INTERVAL);
+        mLocationRequest.setFastestInterval(LOCATION_REQUEST_INTERVAL_MAX);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
 
-            PendingResult<LocationSettingsResult> result =
-                    LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, locationBuilder.build());
+    protected void buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder locationBuilder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
 
-            // TODO: Check if location settings are on. Prompt if not.
+        mLocationSettingsRequest = locationBuilder.build();
+    }
+
+    protected void checkLocationSettings() {
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, mLocationSettingsRequest);
+
+        result.setResultCallback(this);
+    }
+
+    @Override
+    public void onResult(LocationSettingsResult locationSettingsResult) {
+        final Status status = locationSettingsResult.getStatus();
+        switch (status.getStatusCode()) {
+            case LocationSettingsStatusCodes.SUCCESS:
+                Log.i(LOCATION_TAG, "All location settings are satisfied.");
+                startLocationUpdates();
+                break;
+            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                Log.i(LOCATION_TAG, "Location settings are not satisfied. Show the user a dialog to" +
+                        "upgrade location settings ");
+
+                try {
+                    // Show the dialog by calling startResolutionForResult(), and check the result
+                    // in onActivityResult().
+                    status.startResolutionForResult(RegView.this, REQUEST_CHECK_SETTINGS);
+                } catch (IntentSender.SendIntentException e) {
+                    Log.i(LOCATION_TAG, "PendingIntent unable to execute request.");
+                }
+                break;
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                Log.i(LOCATION_TAG, "Location settings are inadequate, and cannot be fixed here. Dialog " +
+                        "not created.");
+                break;
         }
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            // Check for the integer request code originally supplied to startResolutionForResult().
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        Log.i(LOCATION_TAG, "User agreed to make required location settings changes.");
+                        startLocationUpdates();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.i(LOCATION_TAG, "User chose not to make required location settings changes.");
+                        break;
+                }
+                break;
+        }
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient,
+                mLocationRequest,
+                this
+        );
+    }
+
+    @Override
     protected void onStart() {
+        super.onStart();
         // Connect to Google play services
         mGoogleApiClient.connect();
-        super.onStart();
     }
 
     @Override
@@ -70,18 +179,17 @@ public class RegView extends AppCompatActivity implements
 
     @Override
     public void onConnected(Bundle connectionHint) {
-        // Start location updates
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        Log.i(LOCATION_TAG, "Connected to Google Services");
     }
 
     @Override
     public void onConnectionFailed (ConnectionResult result) {
-        Log.i("LOCATION", "Location connection failed.");
+        Log.i(LOCATION_TAG, "Location connection failed.");
     }
 
     @Override
     public void onConnectionSuspended (int i) {
-        Log.i("LOCATION", "Location services suspended. Please reconnect.");
+        Log.i(LOCATION_TAG, "Location services suspended. Please reconnect.");
     }
 
     @Override
@@ -90,22 +198,25 @@ public class RegView extends AppCompatActivity implements
             // Get last known location
             Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
             if (mLastLocation != null) {
-                Log.d("LOCATION", String.valueOf(mLastLocation.getLatitude()));
-                Log.d("LOCATION", String.valueOf(mLastLocation.getLongitude()));
+                Log.d(LOCATION_TAG, String.valueOf(mLastLocation.getLatitude()));
+                Log.d(LOCATION_TAG, String.valueOf(mLastLocation.getLongitude()));
             }
         } catch (SecurityException e) {
             e.printStackTrace();
         }
     }
 
-    public void pressDash(View view)
-    {
+    public void pressDash(View view) {
         Intent intent = new Intent(this, Dashboard.class);
         startActivity(intent);
     }
 
-    public void showHint(View view)
-    {
+    public void showHint(View view) {
         //new view to display number?
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Don't let players press back button
     }
 }
